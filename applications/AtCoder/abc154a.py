3bda1145e6d7e7ab0338caa41465ac32e5429f19
ak2ume <ak2ume@users.noreply.github.com>
2020-02-15 02:33:20 +0900

    added abc154a

---
+++ applications/AtCoder/abc154a.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+def abc154a(s, t, a, b, u):
+    if(s == u):
+        a -= 1
+    else:
+        b -= 1
+    return(a, b)
+
+###
+# main
+
+# input
+(s, t) = input().split()
+(a, b) = map(int, input().split())
+u = input()
+
+(new_a, new_b) = abc154a(s, t, a, b, u)
+
+# output
+print(str(new_a) + " " + str(new_b))
