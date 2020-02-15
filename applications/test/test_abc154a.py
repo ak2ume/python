708a26cdd43f705d15a7d648520faecafc58e7c6
ak2ume <ak2ume@users.noreply.github.com>
2020-02-16 01:02:52 +0900

    refactored test_abc154a.

--- applications/test/test_abc154a.py
+++ applications/test/test_abc154a.py
@@ -7,30 +7,37 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from AtCoder import abc154a
 
+def test_abc154a(input, exp):
+    ngCnt = 0
+    (res_a, res_b) = abc154a.abc154a(input[0], input[1], input[2], input[3], input[4])
+    if(res_a != exp[0]):
+        ngCnt += 1
+        #print("res_a NG!!!")
+    if(res_b != exp[1]):
+        ngCnt += 1
+        #print("res_b NG!!!")
+    return ngCnt
+
+####
+# main
+
 test_result = 0
 
 # test1
-(res_a1, res_b1) = abc154a.abc154a("red", "blue", 3, 4, "red")
-(exp_a1, exp_b1) = (2, 4)
-if(res_a1 != exp_a1):
+input1 = ("red", "blue", 3, 4, "red")
+exp1 = (2, 4)
+if( 0 < test_abc154a(input1, exp1) ):
+    print("test_abc154a pattern1 NG!!!")
     test_result += 1
-    print("test1a NG!!!")
-if(res_b1 != exp_b1):
-    test_result += 1
-    print("test1b NG!!!")
 
 # test2
-(res_a2, res_b2) = abc154a.abc154a("red", "blue", 5, 5, "blue")
-(exp_a2, exp_b2) = (5, 4)
-if(res_a2 != exp_a2):
+input2 = ("red", "blue", 5, 5, "blue")
+exp2 = (5, 4)
+if( 0 < test_abc154a(input2, exp2) ):
+    print("test_abc154a pattern2 NG!!!")
     test_result += 1
-    print("test2a NG!!!")
-if(res_b2 != exp_b2):
-    test_result += 1
-    print("test2b NG!!!")
 
-# finished
 if(test_result == 0):
-    print("test PASSED!!")
+    print("test_abc154a PASSED!!")
 
-print("test finished.")
+print("test_abc154a finished.")
