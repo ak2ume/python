5b00979fafc7568121af70940ceb27da035a7867
ak2ume <ak2ume@users.noreply.github.com>
2020-02-18 18:52:27 +0900

    improved performance

--- applications/AtCoder/abc155c.py
+++ applications/AtCoder/abc155c.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
 
+def calc_insert_index(array, target, left, right):
+    if left == right:
+        if target < array[left]:
+            return left
+        else:
+            return left + 1
+    else:
+        bound = (left + right) >> 1
+        if target < array[bound]:
+            return calc_insert_index(array, target, left, bound)
+        else:
+            return calc_insert_index(array, target, bound+1, right)
+
+
 def abc155c(num, s):
     appearance_dict = {} # 文字列:出現回数
     array = [[] for _ in range(num)] # (出現回数-1)をindexにして文字列を配列で管理する
@@ -9,28 +23,26 @@ def abc155c(num, s):
         target = s[i]
         cnt = 0 # appearance count
 
-        if( target in appearance_dict ):
+        if target in appearance_dict :
             cnt = appearance_dict[target]
-            array[cnt-1].remove(target)
-        appearance_dict[target] = cnt+1
+            array[cnt - 1].remove(target)
+        appearance_dict[target] = cnt + 1
 
-        # sortしながら配列に追加する
+        # 出現回数最大のときのみ2分探索でsortしながら配列に追加する
         insert_idx = 0
-        target_array = array[cnt]
-        if( target_array != [] ):
-            for sort_idx in range(0, len(target_array)):
-                if( target < target_array[sort_idx] ): 
-                    break
-                insert_idx += 1
+        if cnt == max_cnt:
+            if array[cnt]:
+                insert_idx = calc_insert_index(array[cnt], target, 0, len(array[cnt])-1)
         array[cnt].insert(insert_idx, target)
 
-        if( cnt > max_cnt ) : max_cnt = cnt
+        if cnt > max_cnt:
+            max_cnt = cnt
 
-    return(array[max_cnt])
+    return array[max_cnt]
 
 ###
 # main
-if(__name__ == '__main__'):
+if __name__ == '__main__' :
     # input
     num = int(input())
     s = [None for _ in range(num)]
